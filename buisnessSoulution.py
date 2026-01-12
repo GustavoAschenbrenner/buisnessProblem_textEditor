b46933bc6ee5897d812996e9b9fc20fcb0dd5513
GustavoAschenbrenner <gustavoa38@nycstudents.net>
2026-01-12 10:43:22 -0500

    Update buisnessSoulution.py

--- buisnessSoulution.py
+++ buisnessSoulution.py
@@ -18,15 +18,15 @@ root.geometry("1200x600")
 
 # Create buttons
 button_save = Button(root, text="save", background='green',command=save)
-button_check = Button(root, text="auto check", background='skyBlue', command=auto_check)
-button_clear = Button(root, text="clear", background='red',command=clear)
+button_check = Button(root1, text="auto check", background='skyBlue', command=auto_check)
+button_clear = Button(root2, text="clear", background='red',command=clear)
 
 #text box
 text_box = Text(height=600, width=300)
 
 text1 = Text(root,height=5,width=52)
-text2 = Text(root,height=5,width=52)
-text3 = Text(root,height=5,width=52)
+text2 = Text(root1,height=5,width=52)
+text3 = Text(root2,height=5,width=52)
 
 
 # Place widgets in window (with pack function!)
